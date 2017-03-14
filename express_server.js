79fddd080ec10f9249b245b09244e55464cb9222
Leanne <leanneherrndorf@gmail.com>
2017-03-14 23:34:10 +0000

    Added urls_new page and modified express_server to work with it

--- express_server.js
+++ express_server.js
@@ -4,6 +4,9 @@ var PORT = process.env.PORT || 8080; // default port 8080
 
 app.set("view engine", "ejs")
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -23,6 +26,15 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id, urls: urlDatabase };
   res.render("urls_show", templateVars);
@@ -31,6 +43,7 @@ app.get("/urls/:id", (req, res) => {
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
